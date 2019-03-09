93a5b1ae2617129fbe8cd99392528bfec64fa436
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-09 16:41:32 +0200

    Initialize Models Collection In Make Domain Class

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 
 namespace CarSellingSystem.Models
@@ -7,5 +8,11 @@ namespace CarSellingSystem.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; }
+        public Make()
+        {
+            //initialize the models collection to avoid creating new Collection in every call on make class
+            //also avoid null reference exception
+            Models = new Collection<Models>();
+        }
     }
 }
