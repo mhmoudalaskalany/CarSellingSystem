f9488bd706c0ef66ab4d5b02866b3f75e2fba124
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-09 16:03:30 +0200

    add domain model classes

---
+++ Models/Make.cs
@@ -0,0 +1,9 @@
+namespace CarSellingSystem.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+    }
+}
