fab5278e31d48d1d0bb04f36eb5ba5ad1d9de178
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-09 16:11:53 +0200

    adding Context Class

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace CarSellingSystem.Models
 {
     public class Make
---
+++ Persistence/CarContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+namespace CarSellingSystem.Persistence
+{
+    public class CarContext :DbContext
+    {
+        public CarContext(DbContextOptions<CarContext> options):base(options)
+        {
+        }
+    }
+}
