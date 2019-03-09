3d46f06e6f71294663f4456de3f309c02fe502d1
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-09 23:27:37 +0200

    add seed data migration

---
+++ CarSellingSystem/Migrations/20190309211628_add-migration seedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarSellingSystem.Migrations
+{
+    public partial class addmigrationseedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO MAKES (NAME) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO MAKES (NAME) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO MAKES (NAME) VALUES ('Make3')");
+
+
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make1-ModelA',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make1-ModelB',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make1-ModelC',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make1'))");
+
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make2-ModelA',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make2-ModelB',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make2-ModelC',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make2'))");
+
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make3-ModelA',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make3-ModelB',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES ('Make3-ModelC',(SELECT TOP(1) ID FROM MAKES WHERE NAME LIKE 'Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM MAKES WHERE NAME IN ('Make1','Make2','Make3')");
+        }
+    }
+}
