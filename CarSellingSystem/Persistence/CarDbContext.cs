71035acb450b18f5915c2d5c139da5a136978fd9
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-09 23:07:11 +0200

    Add Initial Models

---
+++ CarSellingSystem/Persistence/CarDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarSellingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSellingSystem.Persistence
+{
+    public class CarDbContext : DbContext
+    {
+        public DbSet<Make> Makes { get; set; }
+
+        public CarDbContext(DbContextOptions<CarDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
