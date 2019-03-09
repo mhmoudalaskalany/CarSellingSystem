5d1ad2c03f9ab274c4beda84f497713faf5b2554
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-10 00:06:29 +0200

    add seed features migration

--- CarSellingSystem/Migrations/20190309220448_addFeatureModel.Designer.cs
+++ CarSellingSystem/Migrations/20190309220448_addFeatureModel.Designer.cs
@@ -3,14 +3,16 @@ using CarSellingSystem.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarSellingSystem.Migrations
 {
     [DbContext(typeof(CarDbContext))]
-    partial class CarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190309220448_addFeatureModel")]
+    partial class addFeatureModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarSellingSystem/Migrations/20190309220502_seedFeatures.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarSellingSystem.Migrations
+{
+    public partial class seedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO FEATURES (NAME) VALUES ('FEATURE1')");
+            migrationBuilder.Sql("INSERT INTO FEATURES (NAME) VALUES ('FEATURE2')");
+            migrationBuilder.Sql("INSERT INTO FEATURES (NAME) VALUES ('FEATURE3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM FEATURES");
+        }
+    }
+}
