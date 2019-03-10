7117db3238a00e4e2dbf8cf76dbaf01e21a1a863
mhmoudalaskalany <mhmoudalaskalany@gmail.com>
2019-03-10 14:32:18 +0200

    Add Vehicle Entity And owned Type Contact

---
+++ CarSellingSystem/Models/OwnedTypes/Contact.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSellingSystem.Models.OwnedTypes
+{
+    public class Contact
+    {
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+    }
+}
---
+++ CarSellingSystem/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CarSellingSystem.Models.OwnedTypes;
+
+namespace CarSellingSystem.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        public Contact Contact { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+    }
+}
