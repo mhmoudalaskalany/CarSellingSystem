b91499ef65b231c65666e3a3724a4f436c99545a
mhmoudalaskalany <mhmoudalaskalany@gmail.com>
2019-03-13 14:17:54 +0200

    add vehicle Model

---
+++ CarSellingSystem/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CarSellingSystem.Models.OwnedTypes;
+
+namespace CarSellingSystem.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public Contact Contact { get; set; }
+        public ICollection<Feature> Features { get; set; }
+
+        public Vehicle()
+        {
+            Features = new Collection<Feature>();
+        }
+    }
+}
