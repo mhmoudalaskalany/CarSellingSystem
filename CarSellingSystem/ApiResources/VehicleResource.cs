2583a219ae3dd1d5a3c1ca0bfda67389590c1f3d
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-23 20:37:11 +0200

    add vehicle controller and resource class

---
+++ CarSellingSystem/ApiResources/VehicleResource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CarSellingSystem.ApiResources
+{
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public bool IsRegistered { get; set; }
+        public ContactResource Contact { get; set; }
+        public ICollection<int> Features { get; set; }
+
+        public VehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
---
+++ CarSellingSystem/Controllers/VehiclesController.cs
@@ -0,0 +1,16 @@
+﻿using CarSellingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSellingSystem.Controllers
+{
+    [Route("/api/vehicles")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+        {
+            return Ok(vehicle);
+        }
+    }
+}
