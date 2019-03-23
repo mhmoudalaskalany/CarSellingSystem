f6bf8c10b5206492e2d8af857b99b0b646588ac2
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-23 23:10:45 +0200

    add delete vehicle end point

--- CarSellingSystem/Controllers/VehiclesController.cs
+++ CarSellingSystem/Controllers/VehiclesController.cs
@@ -14,12 +14,12 @@ namespace CarSellingSystem.Controllers
     public class VehiclesController : ControllerBase
     {
         private readonly IMapper _mapper;
-        private readonly CarDbContext _conteext;
+        private readonly CarDbContext _context;
 
         public VehiclesController(IMapper mapper, CarDbContext conteext)
         {
             _mapper = mapper;
-            _conteext = conteext;
+            _context = conteext;
         }
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
@@ -28,8 +28,8 @@ namespace CarSellingSystem.Controllers
                 return BadRequest(modelState:ModelState);
             var vehicle = _mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
-            _conteext.Vehicles.Add(vehicle);
-            await _conteext.SaveChangesAsync();
+            _context.Vehicles.Add(vehicle);
+            await _context.SaveChangesAsync();
             var result = _mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(result);
         }
@@ -39,12 +39,23 @@ namespace CarSellingSystem.Controllers
         {
             if (!ModelState.IsValid)
                 return BadRequest(modelState: ModelState);
-            var vehicle = await  _conteext.Vehicles.Include(v => v.Features).FirstOrDefaultAsync(v => v.Id == id);
+            var vehicle = await  _context.Vehicles.Include(v => v.Features).FirstOrDefaultAsync(v => v.Id == id);
             _mapper.Map<VehicleResource, Vehicle>(vehicleResource,vehicle);
             vehicle.LastUpdate = DateTime.Now;
-            await _conteext.SaveChangesAsync();
+            await _context.SaveChangesAsync();
             var result = _mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle =await  _context.Vehicles.FindAsync(id);
+            if (vehicle == null)
+                return NotFound();
+            _context.Vehicles.Remove(vehicle);
+            await _context.SaveChangesAsync();
+            return Ok(id);
         }
     }
 }
