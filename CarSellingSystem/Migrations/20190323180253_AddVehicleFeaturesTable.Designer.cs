3e68434b37492606e593dc581cbccc32a171844b
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-23 20:03:34 +0200

    add vehicle features table

--- CarSellingSystem/Migrations/20190323180253_AddVehicleFeaturesTable.Designer.cs
+++ CarSellingSystem/Migrations/20190323180253_AddVehicleFeaturesTable.Designer.cs
@@ -4,14 +4,16 @@ using CarSellingSystem.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarSellingSystem.Migrations
 {
     [DbContext(typeof(CarDbContext))]
-    partial class CarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190323180253_AddVehicleFeaturesTable")]
+    partial class AddVehicleFeaturesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarSellingSystem/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using CarSellingSystem.Models.OwnedTypes;
+
+namespace CarSellingSystem.Models
+{
+    [Table("Vehicles")]
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        public Contact Contact { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public Collection<VehicleFeature> Features { get; set; }
+
+        public Vehicle()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
+    }
+}
